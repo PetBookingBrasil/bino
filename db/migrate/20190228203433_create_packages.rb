ed1d3f264f6191582626c768758b134302a746dd
Filipe Fenrich <foolemdev@gmail.com>
2019-04-05 11:33:45 -0300

    Force null: false

--- db/migrate/20190228203433_create_packages.rb
+++ db/migrate/20190228203433_create_packages.rb
@@ -1,8 +1,8 @@
 class CreatePackages < ActiveRecord::Migration[5.2]
   def change
     create_table :packages do |t|
-      t.string :source
-      t.string :destiny
+      t.string :source, null: false
+      t.string :destiny, null: false
       t.string :external_source_id
       t.string :external_destiny_id
       t.integer :status
