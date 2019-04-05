eead71fb649857f97a266e2d6b3eee65cb477314
Lucas Matheus <lucas.passos@petbooking.com.br>
2019-04-05 09:57:04 -0300

    Changes by request

--- app/services/runrunit/runrunit_exporter.rb
+++ app/services/runrunit/runrunit_exporter.rb
@@ -10,13 +10,5 @@ module Runrunit
       response = Runrunit::Resources.new().perform_request(body: body, resource: resource, method: :post)
       Agent.find_by(name: 'zendesk').update(last_sync: DateTime.now)
     end
-
-    private
-
-    def change_status(response)
-      body = { task_status_id: 777343 }
-      resource = "api/v1.0/tasks/#{JSON.parse(response)['id']}/change_status"
-      Runrunit::Resources.new().perform_request(body: body, resource: resource, method: :post)
-    end
   end
 end
---
+++ spec/services/runrunit/runrunit_exporter_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe RunrunitExporter do
+
+  # describe '#export' do
+  #   let(:ticket) { Ticket.create(subject: 'TEST', description: 'TEST') }
+  #   Runrunit::Resources.any_instance.stub(:) { 'Okay' }
+  #   allow_any_instance_of(Runrunit::Resources).to receive(method:, url:, payload:, headers:).and_return('Okay')
+  #
+  #   context 'when is valid to send' do
+  #     it 'calls perform_request' do
+  #       described_class.new(ticket).export
+  #       expect
+  #     end
+  # end
+end
