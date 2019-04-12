9b97958fd7129c56d72fcf78abbc52bc7b984a25
Douglas André <dandrefm@gmail.com>
2019-04-12 16:33:38 -0300

    Adding rspec for enums of BinoPackage

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe BinoPackage, type: :model do
-
   it { should validate_presence_of(:source) }
   it { should validate_presence_of(:destiny) }
 
+  describe 'enums' do
+    it { should define_enum_for(:status)
+        .with_values([:sent, :waiting, :failed])}
+    it { should define_enum_for(:package_type)
+        .with_values([:ticket, :card])}
+  end
 end
