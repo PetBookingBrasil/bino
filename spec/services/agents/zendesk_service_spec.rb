dcc93ef5e0688c3b79367fbbd534b92db6671158
Douglas André <dandrefm@gmail.com>
2019-04-30 16:47:19 -0300

    Adding spec for ZendeskService

--- spec/services/agents/zendesk_service_spec.rb
+++ spec/services/agents/zendesk_service_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Agents::ZendeskService do
 
-  describe ".get" do
-    it "calls response_format" do
-      expect(described_class).to receive(:response_format)
-      described_class.get("runrunit", "ticket", "card")
+  subject { described_class.new(
+    destiny: 'Runrunit',
+    source_type: 'ticket',
+    destiny_type: 'tasks') }
+
+  describe '#get' do
+    it 'returns a formatted response' do
+      expected_response = 'formatted_response'
+      subject.stub(:query) { 'created > 2018-10-10' }
+      stubbed_client = double('A Zendesk API Client')
+      stubbed_client.stub(:search).with(query: 'created > 2018-10-10') { 'results' }
+      subject.stub(:client) { stubbed_client }
+      subject.stub(:format_response).with('tasks', 'results') { expected_response }
+      expect(subject.get).to eq(expected_response)
     end
   end
 end
